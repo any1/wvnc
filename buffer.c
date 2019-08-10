923a17fa354780911e5abc00af28862f2a3840a6
Andri Yngvason <andri.yngvason@marel.com>
2019-08-10 14:07:32 +0000

    buffer: Make inner loops in copy function auto-vectorizable

--- buffer.c
+++ buffer.c
@@ -45,41 +45,33 @@ void buffer_calculate_fb_coords(struct wvnc_output *output,
 
 // TODO: Deduplicate this with the above
 
-#define FB_OFF(name, tx, ty) \
-	static rgba_t *fb_off_##name(rgba_t *fb, uint32_t width, uint32_t height, uint32_t ox, uint32_t oy) \
-	{ \
-		return &fb[(ty) * width + (tx)];\
-	}
-
-FB_OFF(normal, ox, height - oy - 1);
-FB_OFF(90, width - oy - 1, height - ox - 1);
-FB_OFF(180, ox, oy);
-FB_OFF(270, oy, ox);
+#define fb_off_normal(w, h, x, y) ((h) - (y) - 1) * (w) + (x)
+#define fb_off_90(w, h, x, y) ((w) - (y) - 1) * (w) + (h) - (x) - 1
+#define fb_off_180(w, h, x, y) (y) * (w) + (x)
+#define fb_off_270(w, h, x, y) (x) * (w) + (y)
 
 #define COPY_TO_FB(name) \
-static void copy_to_fb_##name(rgba_t *fb, struct wvnc_output *output, struct wvnc_buffer *buffer, \
-							  uint32_t src_x, uint32_t src_y, uint32_t src_w, uint32_t src_h) \
+static void copy_to_fb_##name(rgba_t * __restrict__ fb, \
+							  struct wvnc_output *output, \
+							  struct wvnc_buffer * __restrict__ buffer, \
+							  uint32_t src_x, uint32_t src_y, \
+							  uint32_t src_w, uint32_t src_h) \
 { \
-	for (uint32_t off_y = 0; off_y < src_h; off_y++) { \
-		for (uint32_t off_x = 0; off_x < src_w; off_x++) { \
-			uint32_t x = src_x + off_x; \
-			uint32_t y = src_y + off_y; \
-			uint32_t src = *(uint32_t *)(buffer->data + y*buffer->stride + x * 4); \
+	for (uint32_t y = src_y; y < src_y + src_h; y++) { \
+		for (uint32_t x = src_x; x < src_x + src_w; x++) { \
+			uint32_t *src = ((uint32_t *)buffer->data) + y * buffer->width + x; \
+			rgba_t *tgt = fb + fb_off_##name(output->width, output->height, x, y); \
 			rgba_t c = { \
-				.r = (src >> 16) & 0xff, \
-				.g = (src >>  8) & 0xff, \
-				.b = (src >>  0) & 0xff, \
+				.r = (*src >> 16) & 0xff, \
+				.g = (*src >>  8) & 0xff, \
+				.b = (*src >>  0) & 0xff, \
 				.a = 0xff, \
 			}; \
-			rgba_t *tgt = fb_off_##name( \
-				fb, \
-				output->width, output->height, \
-				x, y \
-			); \
 			*tgt = c; \
 		} \
 	} \
 }
+
 
 COPY_TO_FB(normal);
 COPY_TO_FB(90);
